f3fb4bca619fa276f5242039cd75ff23481835bf
Nate J <jamis.walsh@gmail.com>
2021-01-10 20:31:11 -0800

    Created criteria confirms

--- Develop/script.js
+++ Develop/script.js
@@ -41,21 +41,31 @@ function randomSymbol() {
 function generatePassword() {
 // Prompt password length
   setLength = prompt('Enter password length from 8 to 128 characters')
-  console.log(setLength)
-// if not 8-128, display alert
-
-// if within, begin confirms...
-
-// confirm lowercase
-
-// confirm uppercase
-
-// confirm numbers
-
-// confirm symbols
-
+  console.log(setLength);
 // if no criteria selected, display alert
-
+  if (!setLength) {
+    alert('Please enter a password length between 8 to 128 characters')
+  }
+  // if not 8-128, display alert
+  else if (setLength < 8 || setLength > 128) {
+    setLength = prompt('Length must be between 8 and 128 characters')
+    console.log(setLength);
+  }
+// if within, begin confirms...
+  else {
+// confirm lowercase
+  useLower = confirm('Should password have lowercase letters? Press OK for yes, Cancel for no.')
+  console.log(useLower);
+// confirm uppercase
+  useUpper = confirm('Should password have uppercase letters? Press OK for yes, Cancel for no.')
+  console.log(useUpper);
+// confirm numbers
+  useNumbers = confirm('Should password have numbers? Press OK for yes, Cancel for no.')
+  console.log(useNumbers);
+// confirm symbols
+  useSymbols = confirm('Should password have special characters? Press OK for yes, Cancel for no.')
+  console.log(useSymbols);
+  }
 // display password to field
 
 // Ending of function
