b355ee80016e51c7d297d0ae021bb16ab08d77cc
Nate J <jamis.walsh@gmail.com>
2021-01-14 14:55:06 -0800

    Fixed if statements and added comments

--- script.js
+++ script.js
@@ -1,50 +1,53 @@
-
+// Begin function and prompt user for desired password length
 function generatePassword() {
   setLength = parseInt(prompt('Enter password length from 8 to 128 characters'));
 
   if (setLength < 8 || setLength > 128) {
-    alert("Error")
     return "Password length must be between 8 and 128 characters";
   }
 
- var hasLower = confirm("Should password have lowercase letters? Press OK for yes, Cancel for no");
- var hasUpper = confirm("Should password have uppercase letters? Press OK for yes, Cancel for no");
- var hasNumber = confirm("Should password have numbers? Press OK for yes, Cancel for no");
- var hasSymbol = confirm("Should password have symbols? Press OK for yes, Cancel for no");
+  // Confirms to establish criteria
+  var hasLower = confirm("Should password have lowercase letters? Press OK for yes, Cancel for no");
+  var hasUpper = confirm("Should password have uppercase letters? Press OK for yes, Cancel for no");
+  var hasNumber = confirm("Should password have numbers? Press OK for yes, Cancel for no");
+  var hasSymbol = confirm("Should password have symbols? Press OK for yes, Cancel for no");
 
- var lower = "abcdefghijklmnopqrstuvwxyz";
- var upper= "ABCDEFGHIJKLMOPQRSTUVWXYZ";
- var number = "0123456789";
- var symbol = "~!@#$%^&*()-+";
+  // Set variable strings
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var upper= "ABCDEFGHIJKLMOPQRSTUVWXYZ";
+  var number = "0123456789";
+  var symbol = "~!@#$%^&*()-+";
 
- var password = "";
+  // Blank variables as placeholders for later concatenation
+  var password = "";
 
- var criteria = "";
+  var criteria = "";
+ 
+  //If statements to determine user criteria
+  if (!hasLower && !hasUpper && !hasNumber && !hasSymbol) {
+    return "Password must have one set of criteria"
+  }
+  if (hasLower === true) {
+    criteria += lower;
+  }
+  if (hasUpper === true) {
+    criteria += upper;
+  }
+  if (hasNumber === true) {
+    criteria += number;
+  }
+  if (hasSymbol === true) {
+    criteria += symbol;
+  }
+ 
+  // For loop to determine characters for password
+  for (i = 0; i < setLength; i++) {
+    var randomChar = criteria[Math.floor(Math.random() * criteria.length)];
 
- if (hasLower) {
-  criteria += lower;
- }
- if (hasUpper) {
-  criteria += upper;
- }
- if (hasNumber) {
-  criteria += number;
- }
- if (hasSymbol) {
-  criteria += symbol;
- }
- else {
-   alert("Error")
-   return "Password must have at least one criteria set"
- }
+    password += randomChar;
+  }
 
-for (i = 0; i < setLength; i++) {
-  var randomChar = criteria[Math.floor(Math.random() * criteria.length)];
-
-  password += randomChar;
-}
-
-return password;
+  return password;
 
 }
 
